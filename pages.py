09ac889ff7d8e80e2d20f8493b5ecb756e69c370
Dima <livitorskipunovich@gmail.com>
2022-10-29 13:56:41 +0300

    Update: Add new file "Завдання 2"

--- pages.py
+++ pages.py
@@ -1,6 +1,9 @@
-num = input(' Enter number -> ')
-counter = 0
-for s in num:
-    if s.isdigit():
-        counter+=1
-print(f'Кількість цифр {counter} кількість сімволів {len(num)}')
+try:
+    num = input(' Enter number -> ')
+    counter = 0
+    for s in num:
+        if s.isdigit():
+            counter+=1
+            print(f'Кількість цифр {counter} кількість сімволів {len(num)}')
+except Exception as ex:
+   print(f'Erorr information: {ex}')
