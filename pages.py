4fd1f38d70abdd241364d78f6ace23831cc57d08
Dima <livitorskipunovich@gmail.com>
2022-10-29 13:57:11 +0300

    Update: Add new file "Завдання 3"

--- pages.py
+++ pages.py
@@ -1,3 +1,6 @@
-num = input(' Enter pages -> ')
-q = input("q")
-print(num.count(num))
+try:
+    num = input(' Enter pages -> ')
+    q = input("q")
+    print(num.count(num))
+except Exception as ex:
+   print(f'Erorr information: {ex}')
